d0ce6e7cb73c868765446f9f12c9d609e90e5d6f
Thiraj Pramodya <thiraj.pro@gmail.com>
2023-02-16 11:05:38 +0530

    Repositary created for solving algorithms

---
+++ src/Algorithm/SolarArea.java
@@ -0,0 +1,13 @@
+package Algorithm;
+
+import java.util.Scanner;
+
+public class SolarArea {
+    public void AreaSolver(int x){
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Area of the Solar : ");
+        Double area = scanner.nextDouble();
+        Double a = Math.sqrt(area);
+
+    }
+}
