3f0a30dea0d28a6baf387a13f0309f379be878e5
Thiraj Pramodya <thiraj.pro@gmail.com>
2023-02-16 12:09:39 +0530

    Solved Warmup

--- src/Algorithm/WarmUp.java
+++ src/Algorithm/WarmUp.java
@@ -3,16 +3,10 @@ package Algorithm;
 import java.util.Scanner;
 
 public class WarmUp {
-    public void SolveMeFirst(){
-        Scanner scanner = new Scanner(System.in);
-        System.out.print("Enter a: ");
-         int x = scanner.nextInt();
-        System.out.print("Enter b: ");
-        int y = scanner.nextInt();
-        System.out.println(x + y);
-    }
     public static void main(String[] args) {
-        WarmUp warm = new WarmUp();
-        warm.SolveMeFirst();
+        Scanner scan = new Scanner(System.in);
+        int a = scan.nextInt();
+        int b= scan.nextInt();
+        System.out.println(a+b);
     }
 }
