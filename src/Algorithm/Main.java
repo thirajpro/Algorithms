549549a5838c0ae17f37bff1e779991972300bb1
Thiraj Pramodya <thiraj.pro@gmail.com>
2023-02-17 08:55:21 +0530

    Updated Warmup

--- src/Algorithm/Main.java
+++ src/Algorithm/Main.java
@@ -1,8 +1,31 @@
 package Algorithm;
 
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        SolarArea solar = new SolarArea();
-        solar.AreaSolver();
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Area of the Solar : ");
+        List<Integer> finalArray = new ArrayList<Integer>();
+        int initialArea = scanner.nextInt();
+        int b = 0;
+        b=(int) Math.sqrt(initialArea);
+        int remainingArea =0;
+        for (int i = 0;;i++){
+            finalArray.add(b);
+            if(remainingArea>0){
+                remainingArea = initialArea - (b*b);
+                b = (int) Math.sqrt(remainingArea);
+            }else {
+                break;
+            }
+        }
+        Iterator itr = finalArray.iterator();
+        while(itr.hasNext()){
+            System.out.println(itr.next());
+        }
     }
 }
--- src/Algorithm/SolarArea.java
+++ src/Algorithm/SolarArea.java
@@ -1,14 +1,28 @@
 package Algorithm;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class SolarArea {
     public void AreaSolver(){
         Scanner scanner = new Scanner(System.in);
         System.out.println("Area of the Solar : ");
-        int area = scanner.nextInt();
-        int a = (int) Math.sqrt(area);
-        System.out.println(Math.ceil(a));
+        List<Integer> finalArray = new ArrayList<Integer>();
+        int initialArea = scanner.nextInt();
+        int b = 0;
+        b=(int) Math.sqrt(initialArea);
+        for (int i = 0;;i++){
+            finalArray.add(b);
+            int remainingArea = initialArea - (b*b);
+            b = (int) Math.sqrt(remainingArea);
+            if(remainingArea==0){
+                break;
+            }else {
+                continue;
+            }
+        }
+        System.out.println(finalArray);
 
     }
 }
